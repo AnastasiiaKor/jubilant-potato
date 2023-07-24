8a77e898a6d195bebde576c216968b4d4ca80ddf
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-07-25 03:41:46 +0500

    fixes package

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-// const app = require("./app");
-
-// app.listen(3000, () => {
-//   console.log("Server running on port: 3000");
-// });
 const app = require("./app");
 const mongoose = require("mongoose");
 const { DB_HOST } = process.env;
