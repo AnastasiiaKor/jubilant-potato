aa309937256fdf42ff475b0c2b3ebd6fcc7d39db
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2023-09-12 02:18:57 +0500

    change port

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(3001, () => {
+      console.log("Server running. Use our API on port: 3001");
     });
   })
   .catch((error) => {
